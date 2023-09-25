c6b8fb929e175a2de6a2bfcf628110ff346fb84e
preeti1098 <124133826+preeti1098@users.noreply.github.com>
2023-09-25 10:04:09 +0530

    update code
    update code

---
+++ script.js
@@ -0,0 +1,18 @@
+//console.log('hello')
+
+let clickButton = document.querySelector('.button')
+   
+clickButton.addEventListener('click', function(){
+
+    document.querySelector('.popup').style.display = 'flex'
+})
+
+
+
+let closeButton = document.querySelector('.button-close')
+
+
+closeButton.addEventListener('click', function(){
+
+    document.querySelector('.popup').style.display = 'none'
+})
